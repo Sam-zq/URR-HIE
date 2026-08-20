4f63cefe3e3d0ffa35e3ffe8bb44d951a355ba21
Sam <fupenghui6@gmail.com>
2026-08-20 15:43:41 +0800

    Add files via upload

---
+++ scripts/PatchTST.sh
@@ -0,0 +1,99 @@
+export CUDA_VISIBLE_DEVICES=0
+export TORCH_CUDNN_V8_API_DISABLED=1
+cd /home/zhao/fu/Time-Series-Library-main/
+model_name=PatchTST
+#
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/cite30MW/ \
+  --data_path 3-6.csv \
+  --model_id solar3_96_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 6 \
+  --dec_in 6 \
+  --c_out 6 \
+  --des 'Exp' \
+  --batch_size 16 \
+  --itr 1 \
+  --n_heads 4 \
+  --train_epochs 3
+#
+#python -u run.py \
+#  --task_name long_term_forecast \
+#  --is_training 1 \
+#  --root_path ./dataset/cite30MW/ \
+#  --data_path 3-6.csv \
+#  --model_id solar3_96_192 \
+#  --model $model_name \
+#  --data custom \
+#  --features M \
+#  --seq_len 96 \
+#  --label_len 48 \
+#  --pred_len 192 \
+#  --e_layers 2 \
+#  --d_layers 1 \
+#  --factor 3 \
+#  --enc_in 6 \
+#  --dec_in 6 \
+#  --c_out 6 \
+#  --des 'Exp' \
+#  --itr 1 \
+#  --n_heads 16 \
+#  --train_epochs 3
+#
+#python -u run.py \
+#  --task_name long_term_forecast \
+#  --is_training 1 \
+#  --root_path ./dataset/cite30MW/ \
+#  --data_path 3-6.csv \
+#  --model_id solar3_96_336 \
+#  --model $model_name \
+#  --data custom \
+#  --features M \
+#  --seq_len 96 \
+#  --label_len 48 \
+#  --pred_len 336 \
+#  --e_layers 2 \
+#  --d_layers 1 \
+#  --factor 3 \
+#  --enc_in 6 \
+#  --dec_in 6 \
+#  --c_out 6 \
+#  --des 'Exp' \
+#  --itr 1 \
+#  --n_heads 4 \
+#  --batch_size 128 \
+#  --train_epochs 3
+#
+#python -u run.py \
+#  --task_name long_term_forecast \
+#  --is_training 1 \
+#  --root_path ./dataset/cite30MW/ \
+#  --data_path  3-6.csv \
+#  --model_id solar3_96_720 \
+#  --model $model_name \
+#  --data custom \
+#  --features M \
+#  --seq_len 96 \
+#  --label_len 48 \
+#  --pred_len 720 \
+#  --e_layers 2 \
+#  --d_layers 1 \
+#  --factor 3 \
+#  --enc_in 6 \
+#  --dec_in 6 \
+#  --c_out 6 \
+#  --des 'Exp' \
+#  --itr 1 \
+#  --n_heads 4 \
+#  --batch_size 128 \
+#  --train_epochs 3
